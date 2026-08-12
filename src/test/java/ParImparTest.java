c39f900c16fffad1ba89a8ade26e76ab3f840b33
Felix Armenta <3armenta@gmail.com>
2026-08-12 05:42:42 -0600

    feature/AFR-1203 ParImparTest optimized with BeforeEach

--- src/test/java/ParImparTest.java
+++ src/test/java/ParImparTest.java
@@ -1,4 +1,6 @@
+import org.example.Calculadora;
 import org.example.ParImpar;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -6,17 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParImparTest {
 
+    ParImpar parImparObject;
+
+    @BeforeEach
+    public void setUp(){
+        parImparObject = new ParImpar();
+    }
+
     @Test
     public void evenValidation(){
-        ParImpar parImpar = new ParImpar();
-        boolean resultado = parImpar.esPar(10);
+        boolean resultado = parImparObject.esPar(10);
         assertTrue(resultado);
     }
 
     @Test
     public void oddValidation(){
-        ParImpar parImpar = new ParImpar();
-        boolean resultado = parImpar.esPar(77);
+        boolean resultado = parImparObject.esPar(77);
         assertFalse(resultado);
     }
 }
