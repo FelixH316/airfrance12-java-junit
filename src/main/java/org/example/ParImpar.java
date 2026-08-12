12ba9758eba0e1d7c8ad7d65a1237438a4025296
Felix Armenta <3armenta@gmail.com>
2026-08-12 05:39:44 -0600

    feature/AFR-1202 Even and odd class simplified

--- src/main/java/org/example/ParImpar.java
+++ src/main/java/org/example/ParImpar.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class ParImpar {
     public boolean esPar(int numero){
-        if (numero % 2 == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return numero % 2 == 0;
     }
 }
